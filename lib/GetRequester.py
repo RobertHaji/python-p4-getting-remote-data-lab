bcd00f18ad7f2b11fab3ffaba4c86d5855938584
Robert Haji <roberthadji66@gmail.com>
2025-06-22 00:28:00 +0300

    lab_done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content #Raw Json data
 
     def load_json(self):
-        pass
+        body = self.get_response_body()
+        return json.loads(body) #Converts JSON string to python dict
